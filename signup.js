9c7378ecd1bab8e2370a1a12e7d1fef85954db71
Sweth shaw <116083207+swethshaw@users.noreply.github.com>
2024-02-25 20:25:05 +0530

    upload

---
+++ signup.js
@@ -0,0 +1,47 @@
+function validateform(){
+    var name = document.forms["myform"]["fname"].value;
+    var con = document.forms["myform"]["con"].value;
+    // var em = document.forms["myform"]["email"].value;
+    var pass = document.forms["myform"]["pass"].value;
+    var cp = document.forms["myform"]["cp"].value;
+    var error=pass.localeCompare(cp);
+    if(name.length<5){
+        document.getElementById("formerror").innerHTML= "* The length of name should be more than 5 letter";
+        return false;
+    }
+    if(con.length<10){
+        document.getElementById("formerror").innerHTML= "* The length of contact no. should be 10 digit";
+        return false;
+    }
+    // if(em.length<5){
+    //     document.getElementById("formerror").innerHTML= "* The length of email address should be more than 5 digit";
+    //     return false;
+    // }
+    if(pass.length<10){
+        document.getElementById("formerror").innerHTML= "* The length of password shouldn't be less than 10 digit";
+        return false;
+    }
+    if(cp.length<10){
+        document.getElementById("formerror").innerHTML= "* The length of confirm password shouldn't be less than 10 digit";
+        return false;
+    }
+    if(error!=0){
+        document.getElementById("formerror").innerHTML="* Password and confirm password are not the same.";
+        return false;
+    }
+    else{
+        alert("logging in successfully");
+        return true;
+    }
+}
+function show(){
+    var x=document.getElementById("cp");
+    if(x.type==="password"){
+        x.type="text";
+        document.getElementById('s').value='Hide';
+    }
+    else{
+        x.type="password";
+        document.getElementById('s').value='Show';
+    }
+}
